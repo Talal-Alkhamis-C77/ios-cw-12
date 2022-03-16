27c721d57b6baeb9d297367fc82ddbf9e0cc7f87
Talal-Alkhamis-C77 <70804941+Talal-Alkhamis-C77@users.noreply.github.com>
2022-03-16 20:34:27 +0300

    Meow

--- d5-cw3.playground/Contents.swift
+++ d5-cw3.playground/Contents.swift
@@ -1,3 +1,45 @@
 import UIKit
 
-var greeting = "Hello, playground"
+func username(){
+    print("Your username is Alkhamis_q8")
+}
+username()
+
+func grade(first:Int,second:Int,final:Int) -> String{
+let grade = first + second + final
+    
+    if (grade >= 90 && grade <= 100){
+        return "A"
+    }
+    else if (grade >= 80 && grade <= 89){
+        return "B"
+    }
+    else if (grade >= 70 && grade <= 79){
+        return "C"
+    }
+    else if (grade >= 60 && grade <= 69){
+        return "D"
+    }
+    else {
+        return "f"
+    }
+                
+}
+print(grade(first: 25, second: 60, final: 1))
+
+
+func fullName(Name:String) -> String{
+  return "Talal Alkhamis"
+}
+fullName(Name: "Talal Alkhamis")
+
+func math(First:Int,Second:Int){
+  print(First + Second)
+print(First - Second)
+    print(First * Second)
+    print(First / Second)
+}
+
+math(First: 2, Second: 6)
+
+
